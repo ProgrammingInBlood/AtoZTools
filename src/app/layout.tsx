e79e85be47646c5ab0bfd2c1ff44d96783240d05
ProgrammingInBlood <eklavyasingh12065@gmail.com>
2024-09-22 06:03:31 +0530

    added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Anek_Devanagari } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const Anek = Anek_Devanagari({ subsets: ["latin"] });
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className={Anek.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
